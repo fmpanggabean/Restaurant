fd153d0db8c01c4a496b7a39acebee7cb0d2dd3c
Anggerdhismakusuma <kangger97@gmail.com>
2025-03-20 23:43:27 +0700

    Complete File Menu.java

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,5 +1,34 @@
 package restaurant;
 
-public class Menu {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Menu {
+    private String category;
+    private List<Food> foods;
+
+    public Menu(String category) {
+        this.category = category;
+        this.foods = new ArrayList<>();
+    }
+
+    public void add(Food food) {
+        foods.add(food);
+    }
+
+    public void showItems() {
+        System.out.println(category + ":");
+        for (Food food : foods) {
+            System.out.println("- " + food.getName() + " Rp" + food.getPrice());
+        }
+    }
+
+    public Food getFood(String name) {
+        for (Food food : foods) {
+            if (food.getName().equalsIgnoreCase(name)) {
+                return food;
+            }
+        }
+        return null;
+    }
 }
