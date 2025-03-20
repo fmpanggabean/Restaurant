88d0fc9191b568f1f767b58d6f45df5cb592dc46
Anggerdhismakusuma <kangger97@gmail.com>
2025-03-20 23:42:57 +0700

    Complete File Restaurant.java

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -1,5 +1,61 @@
 package restaurant;
 
-public class Restaurant {
+import java.util.ArrayList;
+import java.util.List;
+import people.Chef;
+import people.Visitor;
 
+public class Restaurant {
+    private String name;
+    private List<Menu> menus;
+    private List<Chef> chefs;
+    private int totalIncome;
+
+    public Restaurant(String name) {
+        this.name = name;
+        this.menus = new ArrayList<>();
+        this.chefs = new ArrayList<>();
+        this.totalIncome = 0;
+    }
+
+    public void addMenu(Menu menu) {
+        menus.add(menu);
+    }
+
+    public void addChef(Chef chef) {
+        chefs.add(chef);
+    }
+
+    public void showMenu() {
+        System.out.println("Menu di " + name + ":");
+        for (Menu menu : menus) {
+            menu.showItems();
+        }
+    }
+
+    public void showChef() {
+        System.out.println("Daftar Chef:");
+        for (Chef chef : chefs) {
+            System.out.println("- " + chef.getName());
+        }
+    }
+
+    public void order(Chef chef, Visitor visitor, String foodName, int quantity) {
+        for (Menu menu : menus) {
+            Food food = menu.getFood(foodName);
+            if (food != null) {
+                int cost = food.getPrice() * quantity;
+                visitor.addToTotal(cost);
+                chef.cook(foodName, quantity);
+                totalIncome += cost;
+                System.out.println(chef.getName() + " memasak " + quantity + "x " + foodName + " untuk " + visitor.getName());
+                return;
+            }
+        }
+        System.out.println("Menu " + foodName + " tidak tersedia.");
+    }
+
+    public void showTotalIncome() {
+        System.out.println("Total pendapatan restoran: Rp" + totalIncome);
+    }
 }
